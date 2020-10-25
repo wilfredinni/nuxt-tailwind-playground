51481ed828e8759a29cd7c9b7a2514a2c9032461
carlos <carlos.w.montecinos@gmail.com>
2020-10-25 18:53:48 -0300

    text field component

---
+++ components/forms/TextField.vue
@@ -0,0 +1,28 @@
+<template>
+  <input class="text-field" type="text" placeholder="jane@example.com" />
+</template>
+
+<script>
+export default {
+  name: 'TextField',
+}
+</script>
+
+<style scoped>
+.text-field {
+  @apply bg-white;
+  @apply border;
+  @apply border-gray-300;
+  @apply rounded-lg;
+  @apply py-2;
+  @apply px-4;
+  @apply block;
+  @apply w-full;
+  @apply appearance-none;
+  @apply leading-normal;
+}
+.text-field:focus {
+  @apply outline-none;
+  @apply shadow-outline;
+}
+</style>
