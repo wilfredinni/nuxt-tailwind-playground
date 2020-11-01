a28f76662f718f71bc0da52e7ceee8b437df5b99
carlos <carlos.w.montecinos@gmail.com>
2020-11-01 20:03:03 -0300

    fixes

--- components/SingleElement.vue
+++ components/SingleElement.vue
@@ -1,55 +1,52 @@
 <template>
-  <div class="flex">
-    <div class="container mx-auto mt-10">
-      <div
-        class="rounded-2xl py-4 transition duration-300 hover:bg-gray-200 w-40 cursor-pointer"
-        @mouseover="showActions()"
-        @mouseleave="hideActions()"
-      >
-        <div class="relative">
-          <div class="absolute ml-8 pt-2">
-            <div
-              class="transition duration-300 w-24 flex justify-between"
-              :class="actionClass"
-            >
-              <!-- star -->
-              <div
-                class="rounded transition duration-300"
-                :class="`hover:bg-${color}-400`"
-                @click="fave = !fave"
-              >
-                <span
-                  class="text-white mx-1 my-2"
-                  :class="{
-                    'mdi mdi-star': fave,
-                    'mdi mdi-star-outline': !fave,
-                  }"
-                />
-              </div>
+  <div
+    class="rounded-2xl py-4 transition duration-300 hover:bg-gray-200 w-40 cursor-pointer"
+    @mouseover="showActions()"
+    @mouseleave="hideActions()"
+  >
+    <div class="relative">
+      <div class="absolute ml-8 pt-2">
+        <div
+          class="transition duration-300 w-24 flex justify-between"
+          :class="actionClass"
+        >
+          <!-- star -->
+          <div
+            class="rounded transition duration-300"
+            :class="`hover:bg-${color}-400`"
+            @click=";(fave = !fave), $emit('fave-action')"
+          >
+            <span
+              class="text-white mx-1 my-2"
+              :class="{
+                'mdi mdi-star': fave,
+                'mdi mdi-star-outline': !fave,
+              }"
+            />
+          </div>
 
-              <!-- menu -->
-              <div
-                class="rounded transition duration-300"
-                :class="`hover:bg-${color}-400`"
-              >
-                <span class="mdi mdi-dots-horizontal text-white mx-1 my-2" />
-              </div>
-            </div>
+          <!-- menu -->
+          <div
+            class="rounded transition duration-300"
+            :class="`hover:bg-${color}-400`"
+          >
+            <span class="mdi mdi-dots-horizontal text-white mx-1 my-2" />
           </div>
         </div>
-
-        <div
-          class="flex items-center justify-center rounded-2xl mx-auto h-32 w-32"
-          :class="`bg-${color}-500`"
-        >
-          <span class="text-white text-6xl" :class="icon" />
-        </div>
-
-        <!-- title -->
-        <div class="flex items-center justify-center">
-          <p class="text-sm pt-2" v-text="title" />
-        </div>
       </div>
+    </div>
+
+    <div
+      class="flex items-center justify-center rounded-2xl mx-auto h-32 w-32"
+      :class="`bg-${color}-500`"
+      @click="$emit('action')"
+    >
+      <span class="text-white text-6xl" :class="icon" />
+    </div>
+
+    <!-- title -->
+    <div class="flex items-center justify-center">
+      <p class="text-sm pt-2" v-text="title" />
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="container mx-auto flex">
+  <div class="container mx-auto flex mt-10">
     <single-element
       v-for="item in elements"
       :key="item.title"
       :title="item.title"
       :icon="item.icon"
       :color="item.color"
+      class="mx-2"
+      @action="clickAction()"
+      @fave-action="faveUnFave()"
     />
   </div>
 </template>
@@ -22,6 +25,14 @@ export default {
         { title: 'Social', icon: 'mdi mdi-glass-mug-variant', color: 'red' },
       ],
     }
+  },
+  methods: {
+    clickAction() {
+      alert('Card Clicked')
+    },
+    faveUnFave() {
+      // alert('Fave or un fave')
+    },
   },
 }
 </script>
