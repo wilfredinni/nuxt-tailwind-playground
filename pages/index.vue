ab4b994f8f8676cc2fc835e2836f97fd7bc06e75
carlos <carlos.w.montecinos@gmail.com>
2020-11-02 21:22:33 -0300

    cols

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container mx-auto flex mt-10">
-    <single-element
-      v-for="item in elements"
-      :key="item.title"
-      :title="item.title"
-      :icon="item.icon"
-      :color="item.color"
-      class="mx-2"
-      @action="clickAction()"
-      @fave-action="faveUnFave()"
-    />
+  <div class="flex justify-center">
+    <div class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+      <div v-for="item in elements" :key="item.title" class="mt-10">
+        <single-element
+          :title="item.title"
+          :icon="item.icon"
+          :color="item.color"
+          class="mx-2"
+          @action="clickAction()"
+          @fave-action="faveUnFave()"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +25,7 @@ export default {
         { title: 'Ahorro', icon: 'mdi mdi-piggy-bank', color: 'blue' },
         { title: 'Comida', icon: 'mdi mdi-food-apple', color: 'red' },
         { title: 'Social', icon: 'mdi mdi-glass-mug-variant', color: 'red' },
+        { title: 'Viajes', icon: 'mdi mdi-airplane', color: 'red' },
       ],
     }
   },
