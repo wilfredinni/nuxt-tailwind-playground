f2cd780aa8f1feb8dfe958f40e2437b6133b80d2
carlos <carlos.w.montecinos@gmail.com>
2020-11-26 14:40:57 -0300

    update dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,10 @@
 <template>
   <div>
-    <div class="grid justify-center mt-10">
-      <!-- title and divider -->
-      <div class="container">
-        <div class="grid grid-cols-5 gap-2">
-          <!-- <div class="col-span-4 bg-green-300"></div> -->
-          <div class="col-span-5 bg-green-300">i</div>
-          <!-- <div class="col-span-1 bg-green-300"></div> -->
-        </div>
-      </div>
-    </div>
-
     <!-- groups -->
     <div class="grid justify-center mt-10">
       <div
         v-if="showFaves"
-        class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mt-4 gap-2"
+        class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mt-4 gap-2"
       >
         <div v-for="item in elements" :key="item.title">
           <single-element
@@ -26,6 +15,18 @@
             @fave-action="faveUnFave()"
           />
         </div>
+      </div>
+    </div>
+
+    <div class="container mx-auto text-center my-4">
+      <div class="grid grid-cols-3 gap-4">
+        <div class="bg-red-300">1</div>
+        <div class="bg-blue-300">2</div>
+        <div class="bg-green-200">3</div>
+        <div class="bg-gray-300">4</div>
+        <div class="bg-orange-300 col-span-2">5</div>
+        <div class="bg-red-300 col-span-3">6</div>
+        <div class="bg-blue-300">7</div>
       </div>
     </div>
   </div>
@@ -42,7 +43,7 @@ export default {
         { title: 'Comida', icon: 'mdi mdi-food-apple', color: 'red' },
         { title: 'Social', icon: 'mdi mdi-glass-mug-variant', color: 'red' },
         { title: 'Viajes', icon: 'mdi mdi-airplane', color: 'red' },
-        { title: 'Viajes', icon: 'mdi mdi-airplane', color: 'red' },
+        { title: 'Viajes2', icon: 'mdi mdi-airplane', color: 'red' },
       ],
     }
   },
