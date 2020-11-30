6f496e269161f55c7a4ce1e8f39da7e367bbeeeb
carlos <carlos.w.montecinos@gmail.com>
2020-11-29 22:31:15 -0300

    bag

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,9 @@
           </div>
         </div>
 
-        <div class="flex flex-1 items-center justify-between h-16 px-8">
+        <div
+          class="flex flex-1 items-center justify-between h-16 px-8 bg-gray-100"
+        >
           <!-- inicio -->
           <h2 class="text-xl">Inicio</h2>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,59 @@
 <template>
-  <div>
-    <!-- groups -->
-    <div class="grid justify-center">
-      <div class="border-b-2 mb-3 flex items-center mx-2">
-        <span class="mdi mdi-menu-down text-2xl text-gray-700" />
-        <p>Favoritos</p>
-      </div>
-
-      <div
-        class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4"
-      >
-        <div v-for="(item, index) in elements" :key="index">
-          <single-element
-            :title="item.title"
-            :icon="item.icon"
-            :color="item.color"
-            @action="clickAction()"
-            @fave-action="faveUnFave()"
-          />
+  <div class="grid justify-center bg-gray-100">
+    <div class="flex shadow-lg mt-3 mb-10 rounded-xl bg-white py-6">
+      <div class="flex flex-1 justify-center">
+        <div class="flex flex-col items-center">
+          <h2 class="text-3xl text-teal-500 font-bold">$1.340.000</h2>
+          <p class="text-xl text-gray-600 font-semibold">Ingresos</p>
         </div>
       </div>
 
-      <!-- pending movements -->
-      <div class="border-b-2 mb-3 flex items-center mx-2 mt-10">
-        <span class="mdi mdi-menu-down text-2xl text-gray-700" />
-        <p>Movimientos pendientes</p>
+      <div class="flex flex-1 justify-center">
+        <div class="flex flex-col items-center">
+          <h2 class="text-3xl text-red-500 font-bold">$340.000</h2>
+          <p class="text-xl text-gray-600 font-semibold">Gastos</p>
+        </div>
       </div>
-      <div class="mx-2">
-        <app-list
-          v-for="(item, index) in urgentMovements"
-          :key="index"
-          :item="item"
+
+      <div class="flex flex-1 justify-center">
+        <div class="flex flex-col items-center">
+          <h2 class="text-3xl text-blue-500 font-bold">$200.000</h2>
+          <p class="text-xl text-gray-600 font-semibold">Ahorro</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- groups -->
+    <div class="border-b-2 mb-3 mt-3 flex items-center mx-2">
+      <span class="mdi mdi-menu-down text-2xl text-gray-700" />
+      <p>Favoritos</p>
+    </div>
+
+    <div
+      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4"
+    >
+      <div v-for="(item, index) in elements" :key="index">
+        <single-element
+          :title="item.title"
+          :icon="item.icon"
+          :color="item.color"
+          @action="clickAction()"
+          @fave-action="faveUnFave()"
         />
       </div>
+    </div>
+
+    <!-- pending movements -->
+    <div class="border-b-2 mb-3 flex items-center mx-2 mt-10">
+      <span class="mdi mdi-menu-down text-2xl text-gray-700" />
+      <p>Movimientos pendientes</p>
+    </div>
+    <div class="mx-2">
+      <app-list
+        v-for="(item, index) in urgentMovements"
+        :key="index"
+        :item="item"
+      />
     </div>
   </div>
 </template>
