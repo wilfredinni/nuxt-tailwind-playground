cca4bf73aa8b10aecd6ba860243edbd568e99d94
wilfredinni <carlos.w.montecinos@gmail.com>
2020-12-02 18:39:20 -0300

    fix layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="grid justify-center">
-    <!-- groups -->
-    <div class="border-b-2 mb-3 mt-3 flex items-center mx-2">
-      <span class="mdi mdi-menu-down text-2xl text-gray-700" />
-      <p>Favoritos</p>
-    </div>
+  <div class="flex justify-center">
+    <div class="flex flex-col lg:w-5/6" :class="columnSize">
+      <!-- groups -->
+      <div class="border-b-2 mb-3 mt-3 flex items-center mx-2">
+        <span class="mdi mdi-menu-down text-2xl text-gray-700" />
+        <p>Favoritos</p>
+      </div>
 
-    <div
-      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4"
-    >
-      <div v-for="(item, index) in elements" :key="index">
-        <single-element
-          :title="item.title"
-          :icon="item.icon"
-          :color="item.color"
-          @action="clickAction()"
-          @fave-action="faveUnFave()"
+      <div
+        class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5"
+      >
+        <div v-for="(item, index) in elements" :key="index">
+          <single-element
+            :title="item.title"
+            :icon="item.icon"
+            :color="item.color"
+            @action="clickAction()"
+            @fave-action="faveUnFave()"
+          />
+        </div>
+      </div>
+
+      <!-- pending movements -->
+      <div class="border-b-2 mb-3 flex items-center mx-2 mt-10">
+        <span class="mdi mdi-menu-down text-2xl text-gray-700" />
+        <p>Movimientos pendientes</p>
+      </div>
+      <div class="mx-2">
+        <app-list
+          v-for="(item, index) in urgentMovements"
+          :key="index"
+          :item="item"
         />
       </div>
-    </div>
-
-    <!-- pending movements -->
-    <div class="border-b-2 mb-3 flex items-center mx-2 mt-10">
-      <span class="mdi mdi-menu-down text-2xl text-gray-700" />
-      <p>Movimientos pendientes</p>
-    </div>
-    <div class="mx-2">
-      <app-list
-        v-for="(item, index) in urgentMovements"
-        :key="index"
-        :item="item"
-      />
     </div>
   </div>
 </template>
@@ -97,6 +99,12 @@ export default {
       ],
       sizes: ['lg', 'md', 'sm'],
     }
+  },
+  computed: {
+    columnSize() {
+      if (this.elements.length > 0) return 'xl:w-auto'
+      return 'xl:w-2/4'
+    },
   },
   methods: {
     clickAction() {
